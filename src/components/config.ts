1c7a119cb480cf063b872a1edbbf574245824052
Sean Newell <sean@thenewells.us>
2023-07-19 12:57:03 +0100

    PLAT-24148 Ensure auto pass works on a fresh learner

--- src/components/config.ts
+++ src/components/config.ts
@@ -1,8 +1,9 @@
 export const BRAND = "bmFydXRvOjE="; // naruto:1 => b64 encoded
 export const PORTAL_URL = "https://naruto-konohalms.stagingadministratelms.com";
 export const EVENT_ID = "Q291cnNlOjE1Mg=="; // Course#152
+// tracking identifier = 9001
 export const SIGNED_JWT =
-  "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDY0Iiwic3ViX3R5cGUiOiJ0cmFja2luZ19pZCIsImlhdCI6MTY3NjMwMTI2MiwiZXhwIjoxODk3MjI5NjYyfQ.kg0Ta6nNN2C7O3djlB9wfN-VHt9s2UGRCoRW89hIbJ7J-I95ceIxRn9fU4hC3PAg4nRtHbGC3HXCYt5PMUPrlg";
+  "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5MDAxIiwic3ViX3R5cGUiOiJ0cmFja2luZ19pZCIsImlhdCI6MTY3NjMwMTI2MiwiZXhwIjoxODk3MjI5NjYyfQ.xq2owjbevGOsPxU_H2-DWwJAuD4R2r5JdbqnDgNP8yF0U8kAt9rK6RNZVI2f1DnTjfQNcjVmeU5cbTo4QblizA";
 
 export const ITEMS = [
   {
